ad3ffb19968d751fa157646eee667fe420f3ec1a
essian <jessicampdennis@hotmail.com>
2016-07-15 20:02:52 +0100

    starts html parser

--- html_struct.rb
+++ html_struct.rb
@@ -4,7 +4,7 @@ def parse_tag(string)
   tag = {}
   
   # save tag after < character
-  tag_type = string.match(/<(\w+) /)
+  tag_type = string.match(/<(\w+)(?:>| )/)
 
   # saves either side of an equal sign
   attributes = string.scan(/(\w+)\s*=\s*['"](.*?)['"]/)
@@ -24,12 +24,24 @@ end
 # p tag = parse_tag("<p class=\"foo bar\" id='baz' src = 'hello' repeat>")
 
 # p tag = parse_tag("<img src='http://www.example.com' title = 'funny things' repeat>")
-p tag = parse_tag("<div>  div text before  <p>    p text  </p>  <div>    more div text  </div>  div text after</div>"
+# p tag = parse_tag("<div>  div text before  <p>    p text  </p>  <div>    more div text  </div> div text after</div>"
 )
 
 Node = Struct.new(:data, :children, :parent)
 
 def parser_script(string)
+  make_node
+   iterate through string
+   tag = []
+    when < is hit, 
+      go up one level if next char is /
+      start pushing to tag
+    when > is hit, stop pushing to tag
+    send tag to tag_parser
+    create parsed_tag Node
+    send node to tree_builder
+    make_node(children)
+  end
 
 
 end
