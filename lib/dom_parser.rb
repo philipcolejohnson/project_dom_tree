b7f3d9dc0a317c60fdaa8dc6ff577b94f6805a32
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-07-16 15:07:26 -0700

    Creates HTML tree

--- lib/dom_parser.rb
+++ lib/dom_parser.rb
@@ -1,25 +1,31 @@
 Node = Struct.new(:data, :children, :parent)
 
 class DomParser
+  TAG_TYPE_REGEX = /<[\/|!]?(\w+)(?:>| )/
+  TAG_ATTR_REGEX = /(\w+)\s*=\s*['"](.*?)['"]/
+  HTML_REGEX = /((?<=>).*?(?=<)|<.*?>)/
+
   def initialize
     #@html = html
+    @elements = []
   end
 
   def parser_script(string)
-    @elements = string.scan(/((?<=>).*?(?=<)|<.*?>)/)
+    # save everything, split by tags or text
+    string.scan(HTML_REGEX).each { |item| @elements << item[0] }
   end
 
   def parse_tag(string)
     tag = {}
 
     # save tag after < character
-    tag_type = string.match(/<(\w+)(?:>| )/)
+    tag_type = string.match(TAG_TYPE_REGEX)
 
     # saves either side of an equal sign
-    attributes = string.scan(/(\w+)\s*=\s*['"](.*?)['"]/)
+    attributes = string.scan(TAG_ATTR_REGEX)
 
-    #options
-    options = string.scan(/\s*(\w)\s*[?!==]/)
+    #options (TODO)
+    # options = string.scan(/\s*(\w)\s*[?!==]/)
 
     tag[:type] = tag_type[1]
     attributes.each do |attribute|
@@ -29,43 +35,69 @@ class DomParser
     tag
   end
 
-  def create_tree
-    parser_script(@html)
-    @root = create_node(@elements[0])
-    # current_node = @root
-    make_children(@root, 1)
+  def create_tree(html)
+    parser_script(html)
+    if tag?(@elements[0])
+      first = parse_tag(@elements[0]) 
+    else
+      raise "Not an HTML document."
+    end
+    begin_process(first)
   end
 
-  def create_node(tag, parent = nil)
-    Node.new(tag, nil, parent)
+  def begin_process(first)
+    if first[:type] == "DOCTYPE"
+      @root = create_node(parse_tag(@elements[1]))
+      make_children(@root, 2)
+    else
+      @root = create_node(first)
+      make_children(@root, 1)
+    end
+  end
+
+  #creates a node with no children and an optional parent
+  def create_node(data, parent = nil)
+    Node.new(data, [], parent)
   end
 
 
-  def make_children(parent, tag_index)
+  def make_children(parent, tag_index) 
 
-    current_node = parent
-
-
-
-    # return if the next one is a closing tag?
-    loop do 
-      return if closing_tag_for?(@elements[tag_index, parent])
-      # if next one is a tag
+    loop do
+      return true if @elements[tag_index].nil?
+      return tag_index + 1 if closing_tag_for?(@elements[tag_index], parent)
+    
       if opening_tag?(@elements[tag_index])
-        create_node(parse_tag(@elements[tag_index]), parent)
+        new_child = create_tag_child(@elements[tag_index], parent)
+        tag_index = make_children(new_child, tag_index + 1)
+      elsif !tag?(@elements[tag_index])
+        create_text_child(@elements[tag_index], parent)
+        tag_index += 1
+      else
+        # we've hit an unexpected closing tag
+        raise "Closing tag #{@elements[tag_index]} is incorrectly placed"
       end
     end
-    
-    # create a node
-    # make_children(node)
-    # else
-    # create a node
-    # loops until we hit closing tag
+
   end
 
-  def closing_tag_for?(current_tag, parent_node)
-    #check if current tag is closing_tag && current_tag.type = parent_node.type.
+  #accepts a string and a parent node and sees string is the closing tag
+  def closing_tag_for?(current_element, parent_node)
+    if tag?(current_element)
+      return false if !closing_tag?(current_element)
+      current_element = parse_tag(current_element)
+      current_element[:type] == parent_node.data[:type]
+    end
+  end
 
+  def create_tag_child(string, parent)
+    new_child = create_node(parse_tag(string), parent)
+    parent.children << new_child
+    new_child
+  end
+
+  def create_text_child(string, parent)
+    parent.children << create_node(string, parent)
   end
 
   def opening_tag?(tag)
@@ -74,5 +106,9 @@ class DomParser
 
   def closing_tag?(element)
     element[1] == "/" 
+  end
+
+  def tag?(string)
+    string[0] == "<"
   end
 end
--- spec/dom_parser_spec.rb
+++ spec/dom_parser_spec.rb
@@ -61,4 +61,51 @@ describe DomParser do
       expect(subject.opening_tag?(tag)).to be(false)
     end
   end
+
+  describe "#closing_tag_for?" do
+    it "identifies a closing tag pair" do
+      opening_node = double(data: { type: "p" } )
+      closing = "</p>"
+      # allow(subject).to receive(:closing_tag?).and_return(true)
+      expect(subject.closing_tag_for?(closing, opening_node))
+        .to be(true)
+    end
+
+    it "returns false for an unmatched pair" do
+      opening_node = double(data: { type: "p" } )
+      closing = "</img>"
+      # allow(subject).to receive(:closing_tag?).and_return(true)
+      expect(subject.closing_tag_for?(closing, opening_node))
+        .to be(false)
+    end
+  end
+
+  describe "#make_children" do
+    let(:parent) { subject.create_node(subject.parse_tag("<p>")) }
+    let(:current_node) { double(elements: ["hi", "<img>", "</p>"] ) }
+
+    it "creates child nodes of tags"
+
+    it "creates child nodes of text"
+
+    it "can make children of both text and tags"
+
+    it "recursively calls #make_children on tag children" do
+
+      allow(current_node).to receive(:closing_tag_for?).and_return(true, false)
+      allow(current_node).to receive(:parse_tag)
+        .and_return({ type: "img" }, { type: "img" }, { type: "p" }, { type: "p" })
+      allow(current_node).to receive(:make_children).and_return(2 , 3)
+      # allow(current_node).to receive(:elements).and_return("<p>")
+      expect(current_node.children[0]).to receive(:make_children)
+      current_node.make_children(parent, 0)
+    end
+
+    it "stops when it hits a closing tag match"
+  end
 end
+
+# load './lib/dom_parser.rb'
+# d = DomParser.new
+# html = "<div>  div text before  <p>    p text  </p>  <div>    more div text  </div>  div text after</div>"
+# d.create_tree(html)
